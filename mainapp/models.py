e56c8c6612af199e25ab2d2a36f45297495435e2
liveseyy <dima74ermilov@gmail.com>
2020-12-16 14:51:21 +0500

    Изменил архитектуру

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
 User = get_user_model()
 
@@ -36,12 +37,17 @@ class Product(models.Model):
     def __str__(self):
         return self.title
 
+    class Meta:
+        abstract = True
+
 
 class CartProduct(models.Model):
     """Модель, связывающая корзины и находящиеся в них товары"""
     user = models.ForeignKey('Customer', verbose_name="Покупатель", on_delete=models.CASCADE)
     cart = models.ForeignKey('Cart', verbose_name="Корзина", on_delete=models.CASCADE, related_name="related_products")
-    product = models.ForeignKey(Product, verbose_name="Товар", on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey("content_type", "object_id")
     qty = models.PositiveIntegerField(default=1)
     final_price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name="Общая цена")
 
@@ -68,13 +74,3 @@ class Customer(models.Model):
 
     def __str__(self):
         return f"Покупатель: {self.user.first_name} {self.user.last_name}"
-
-
-class Specification(models.Model):
-    """Характеристика"""
-    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
-    object_id = models.PositiveIntegerField()
-    name = models.CharField(max_length=255, verbose_name="Имя товара для характеристик")
-
-    def __str__(self):
-        return f"Характеристики для товара: {self.name}"
