c816c282c6dd0b30439a64687d30cf3c8b8431de
liveseyy <dima74ermilov@gmail.com>
2020-12-29 11:05:50 +0500

    Добавил отображение корзины и её содержимого

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.BaseView.as_view(), name='base'),
     path('products/<str:ct_model>/<str:slug>/', views.ProductDetailView.as_view(), name='product_detail'),
     path('category/<str:slug>/', views.CategoryDetailView.as_view(), name='category_detail'),
+    path('cart/', views.CartView.as_view(), name='cart'),
 ]
