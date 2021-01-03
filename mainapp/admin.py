248bbf188716feea34eec2c02fc3cfd16ab13c8c
liveseyy <dima74ermilov@gmail.com>
2021-01-03 12:32:38 +0500

    Добавил систему пользователей и страницу пользователя с заказами

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,44 +1,6 @@
 from django.contrib import admin
-from django.forms import ModelChoiceField, ModelForm
 
 from .models import *
-
-
-class SmartphoneAdminForm(ModelForm):
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        instance = kwargs.get('instance')
-        if instance and not instance.sd:
-            self.fields['sd_volume_max'].widget.attrs.update({
-                'readonly': True, 'style': 'background: lightgray'
-            })
-
-    def clean(self):
-        if not self.cleaned_data['sd']:
-            self.cleaned_data['sd_volume_max'] = None
-        return self.cleaned_data
-
-
-@admin.register(Notebook)
-class NotebookAdmin(admin.ModelAdmin):
-
-    def formfield_for_foreignkey(self, db_field, request, **kwargs):
-        if db_field.name == "category":
-            return ModelChoiceField(Category.objects.filter(slug="notebooks"))
-        return super().formfield_for_foreignkey(db_field, request, **kwargs)
-
-
-@admin.register(Smartphone)
-class SmartphoneAdmin(admin.ModelAdmin):
-
-    change_form_template = 'admin.html'
-    form = SmartphoneAdminForm
-
-    def formfield_for_foreignkey(self, db_field, request, **kwargs):
-        if db_field.name == "category":
-            return ModelChoiceField(Category.objects.filter(slug="smartphones"))
-        return super().formfield_for_foreignkey(db_field, request, **kwargs)
 
 
 admin.site.register(Category)
@@ -46,3 +8,4 @@ admin.site.register(CartProduct)
 admin.site.register(Cart)
 admin.site.register(Order)
 admin.site.register(Customer)
+admin.site.register(Product)
